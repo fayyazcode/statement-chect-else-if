dd6291a4f571c07f1497b695f53ed31dfeca0014
Fayyaz Alam <fayazalam.98@gmail.com>
2021-04-21 04:55:59 -0700

    statement check Question 6

--- app.js
+++ app.js
@@ -92,3 +92,23 @@
     // else{
     //     alert("Incorrect Password")
     // }
+    
+
+
+
+    //Q6*******************************
+
+//     This if/else statement does not work. Try to fix it:
+// var greeting;
+// var hour = 13;
+// if (hour < 18) {
+// greeting = "Good day";
+// alert(greeting)
+// }
+// else{
+// greeting = "Good evening";
+// alert(greeting)
+// }
+// //brackets and alert added to make statement work
+
+//Q7*****************************
