d800947fd93304608fd1298b2cf07a5ea2c00f15
Fayyaz Alam <fayazalam.98@gmail.com>
2021-04-21 05:02:23 -0700

    4 hrs clock check

--- app.js
+++ app.js
@@ -112,3 +112,18 @@
 // //brackets and alert added to make statement work
 
 //Q7*****************************
+
+// var time=+prompt("Enter time in 24 hrs clock format")
+
+// if(time>=0000 &&time<1200){
+//     alert("Good Morning")
+// }
+// else if(time>=1200 && time<1700){
+//     alert("good afternoon")
+// }
+// else if(time>=1700&&time<2100){
+//     alert("good evening")
+// }
+// else if(time>=2100&&time<2359){
+//     alert("Good Night")
+// }
