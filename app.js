28dde7433fe7da5539339442883387e3475e8560
Fayyaz Alam <fayazalam.98@gmail.com>
2021-04-21 02:55:37 -0700

    if else uperr lower and number check

---
+++ app.js
@@ -0,0 +1,23 @@
+
+//if else statement check
+
+//Q1***************************************
+
+
+// var input=prompt("enter somethung")
+// if(input>="A"&&input<="Z"){
+//  alert("Upper Case")
+// }
+// else if(input>="a"&&input<="z"){
+//     alert("lower Case")
+//    }
+// else if(input<=0||input>=0){
+//     alert("its a number")
+// }
+// else{
+//     alert("special character")
+// }
+
+
+//Q2********************************************
+
